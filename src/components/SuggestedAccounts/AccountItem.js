12e9b35da99391a0eef881258090d7aa5c49d5bd
Đào Duy An <daoduyan8801@gmail.com>
2023-05-02 20:23:53 +0700

    Xây dựng UI phần sidebar #3: Account preview

--- src/components/SuggestedAccounts/AccountItem.js
+++ src/components/SuggestedAccounts/AccountItem.js
@@ -1,27 +1,45 @@
 import classNames from 'classnames/bind';
+import Tippy from '@tippyjs/react/headless';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
+import AccountPreview from './AccountPreview/AccountPreview';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './SuggestedAccounts.module.scss';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
-    return (
-        <div className={cx('account-item')}>
-            <img
-                className={cx('avatar')}
-                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/f359ad9b23ffbeff595ff074fa6082b2~c5_100x100.jpeg?x-expires=1683187200&x-signature=rz5Mp2cKXZjcfgBijhVJvf%2BYUQg%3D"
-                alt=""
-            />
-
-            <div className={cx('item-info')}>
-                <p className={cx('nickname')}>
-                    <strong>quocnguyenphu</strong>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-                </p>
-                <p className={cx('name')}>Quốc Nguyễn Phú</p>
+    const renderPreview = (props) => {
+        return (
+            <div tabIndex="-1" {...props}>
+                <PopperWrapper>
+                    <AccountPreview />
+                </PopperWrapper>
             </div>
+        );
+    };
+
+    return (
+        // Uing a wrapper <div> tag around the reference element solves this
+        // by creating a new parentNode context.
+        <div>
+            <Tippy interactive delay={[800, 0]} placement="bottom" offset={[-20, 0]} render={renderPreview}>
+                <div className={cx('account-item')}>
+                    <img
+                        className={cx('avatar')}
+                        src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/f359ad9b23ffbeff595ff074fa6082b2~c5_100x100.jpeg?x-expires=1683187200&x-signature=rz5Mp2cKXZjcfgBijhVJvf%2BYUQg%3D"
+                        alt=""
+                    />
+                    <div className={cx('item-info')}>
+                        <p className={cx('nickname')}>
+                            <strong>quocnguyenphu</strong>
+                            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                        </p>
+                        <p className={cx('name')}>Quốc Nguyễn Phú</p>
+                    </div>
+                </div>
+            </Tippy>
         </div>
     );
 }
---
+++ src/components/SuggestedAccounts/AccountPreview/AccountPreview.js
@@ -0,0 +1,40 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+import Button from '~/components/Button';
+import styles from './AccountPreview.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountPreview() {
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('header')}>
+                <img
+                    className={cx('avatar')}
+                    src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/f359ad9b23ffbeff595ff074fa6082b2~c5_100x100.jpeg?x-expires=1683187200&x-signature=rz5Mp2cKXZjcfgBijhVJvf%2BYUQg%3D"
+                    alt=""
+                />
+                <Button className={cx('follow-btn')} primary>
+                    Follow
+                </Button>
+            </div>
+            <div className={cx('body')}>
+                <p className={cx('nickname')}>
+                    <strong>quocnguyenphu</strong>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </p>
+                <p className={cx('name')}>Quốc Nguyễn Phú</p>
+                <p className={cx('analytics')}>
+                    <strong className={cx('value')}>8.2M </strong>
+                    <span className={cx('label')}>Followers</span>
+                    <strong className={cx('value')}>8.2M </strong>
+                    <span className={cx('label')}>Likes</span>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default AccountPreview;
